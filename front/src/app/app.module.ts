69cd989d841be1c920c7d29e40b849f9b1d06d4a
cinth <votacinthia@gmail.com>
2023-08-16 16:49:35 -0300

    componente footer - ver imagenes

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NavComponent// lo cambie de declarations a imports
+    NavComponent,// lo cambie de declarations a imports,
+    FooterComponent // lo cambie de declarations a imports,
   ],
   providers: [],
   bootstrap: [AppComponent]
