4d6d748c19a13e25a00391f8f4358674d31341a1
Juan Manuel Camilo <juanma.camilo.27@gmail.com>
2023-08-25 13:51:57 -0300

    Le saque el standalone al NAV

--- front/src/app/components/nav/nav.component.ts
+++ front/src/app/components/nav/nav.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
-  standalone: true,
-  
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
-export class NavComponent {
-
-}
+export class NavComponent {}
