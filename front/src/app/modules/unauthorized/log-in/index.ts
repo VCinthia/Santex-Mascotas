2777683d55800cdefe6a3fc8419945f3ab6df74b
Juan Manuel Camilo <juanma.camilo.27@gmail.com>
2023-09-07 11:22:07 -0300

    Actualizacion de barrel

--- front/src/app/modules/unauthorized/log-in/index.ts
+++ front/src/app/modules/unauthorized/log-in/index.ts
@@ -1,3 +1,5 @@
 export * from './components';
 export * from './components/log-in-form';
+export * from './log-in-routing.module';
 export * from './log-in.component';
+export * from './log-in.module';
