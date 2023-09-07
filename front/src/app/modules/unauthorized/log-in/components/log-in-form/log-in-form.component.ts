c455f27b2156128b57c0011f77264215f57c5d37
Juan Manuel Camilo <juanma.camilo.27@gmail.com>
2023-09-07 11:19:03 -0300

    Hice standalone al componente que contiene el form

--- front/src/app/modules/unauthorized/log-in/components/log-in-form/log-in-form.component.ts
+++ front/src/app/modules/unauthorized/log-in/components/log-in-form/log-in-form.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-log-in-form',
   templateUrl: './log-in-form.component.html',
-  styleUrls: ['./log-in-form.component.css']
+  styleUrls: ['./log-in-form.component.css'],
 })
-export class LogInFormComponent {
-
-}
+export class LogInFormComponent {}
