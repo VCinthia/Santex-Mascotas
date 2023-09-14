4c014e808f1f5d62c8c5e226695a77088e8892bf
Juan Manuel Camilo <juanma.camilo.27@gmail.com>
2023-09-14 14:10:31 -0300

    Importe el modulo del carousel

--- front/src/app/modules/unauthorized/log-in/log-in.module.ts
+++ front/src/app/modules/unauthorized/log-in/log-in.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { LogInComponent } from './log-in.component';
 import { LogInFormComponent } from './components';
 import { LogInRoutingModule } from './log-in-routing.module';
+import { CarouselModule } from '../../carousel/carousel.module';
 
 @NgModule({
   declarations: [LogInComponent],
-  imports: [CommonModule, LogInFormComponent, LogInRoutingModule],
+  imports: [
+    CommonModule,
+    LogInFormComponent,
+    LogInRoutingModule,
+    CarouselModule,
+  ],
 })
 export class LogInModule {}
