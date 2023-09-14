cca6ed398e9c00b2b10afcc4c557fd215272b615
Juan Manuel Camilo <juanma.camilo.27@gmail.com>
2023-09-14 14:11:46 -0300

    Importe el modulo del carousel

--- front/src/app/modules/unauthorized/register/register.module.ts
+++ front/src/app/modules/unauthorized/register/register.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register.component';
 import { RegisterFormComponent } from './components';
 import { RegisterRoutingModule } from './register-routing.module';
+import { CarouselModule } from '../../carousel/carousel.module';
 
 @NgModule({
   declarations: [RegisterComponent],
-  imports: [CommonModule, RegisterFormComponent, RegisterRoutingModule],
+  imports: [
+    CommonModule,
+    RegisterFormComponent,
+    RegisterRoutingModule,
+    CarouselModule,
+  ],
 })
 export class RegisterModule {}
