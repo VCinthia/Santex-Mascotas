a35509bdfcd75839145c6e288d5f0fde1ca0a916
Juan Manuel Camilo <juanma.camilo.27@gmail.com>
2023-09-21 12:27:10 -0300

    Importe el RouterModule

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [AppComponent, NavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule, // lo cambie de declarations a imports,
+    RouterModule,
     FooterComponent, // lo cambie de declarations a imports,
   ],
   providers: [],
