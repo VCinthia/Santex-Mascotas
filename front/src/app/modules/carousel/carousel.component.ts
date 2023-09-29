89b4b85422f7dfb4f546e50fa4af02b5bb1dd753
Juan Manuel Camilo <juanma.camilo.27@gmail.com>
2023-09-29 01:58:09 -0300

    Agregue un evento que se dispara cuando la ventana cambia de tamanno. Hace que el carousel no aparezca a partir de cierta cantidad de pixeles

--- front/src/app/modules/carousel/carousel.component.ts
+++ front/src/app/modules/carousel/carousel.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
 })
 export class CarouselComponent {
-
+  public screenSize = true;
+  ngOnInit() {
+    window.onresize = () => (this.screenSize = window.innerWidth > 1100);
+  }
 }
