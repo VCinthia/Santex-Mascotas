0d437b518b78bb096d91c68aa8018d0df2d68710
Juan Manuel Camilo <juanma.camilo.27@gmail.com>
2023-10-05 11:53:27 -0300

    Utilice un metodo para traer el dni del usuario y pasarlo como parametro al metodo de getMascotByDni

--- front/src/app/modules/tus-mascotas/tus-mascotas.component.ts
+++ front/src/app/modules/tus-mascotas/tus-mascotas.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MascotaService } from 'src/app/services/mascota.service';
+import { TokenService } from 'src/app/services/token.service';
+import { MascotasDTO } from 'src/app/models/mascotas.dto';
+
 @Component({
   selector: 'app-tus-mascotas',
   templateUrl: './tus-mascotas.component.html',
-  styleUrls: ['./tus-mascotas.component.css']
+  styleUrls: ['./tus-mascotas.component.css'],
 })
-export class TusMascotasComponent implements OnInit{
+export class TusMascotasComponent implements OnInit {
+  dniPersona: number | any = null;
+
+  constructor(
+    public mascotasService: MascotaService,
+    public tokenService: TokenService
+  ) {}
+
+  mascotasDTO: MascotasDTO | any = null;
+  mascotas: MascotasDTO[] = [];
 
   ngOnInit(): void {
-    
+    this.dniPersona = this.tokenService.getDniUsuario();
+    this.mascotasService.getMascotaByDni(this.dniPersona).subscribe(
+      (data) => {
+        this.mascotas = data;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 
-  onEditar(){}
-
+  onEditar() {}
 }
